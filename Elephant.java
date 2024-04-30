676465786f413e2d85092ff2d36b2ddc1be1fef5
Dashanesta <71793564+Dashanesta@users.noreply.github.com>
2024-04-30 14:40:54 -0400

    Elephant eats apple

--- Elephant.java
+++ Elephant.java
@@ -14,6 +14,29 @@ public class Elephant extends Actor
      */
     public void act()
     {
-        move(1);
+        if(Greenfoot.isKeyDown("left"))
+        {
+            move(-1);
+        }
+        else if (Greenfoot.isKeyDown("right"))
+        {
+            move(1);
+        }
+        
+        // Remove apple if elephant eats it
+        eat();
+    }
+    
+    /**
+     * Eat the apple and spawn new apple if an apple is eaten.
+     */
+    public void eat()
+    {
+        if(isTouching(Apple.class))
+        {
+            removeTouching(Apple.class);
+            MyWorld world = (MyWorld) getWorld();
+            world.createApple();
+        }
     }
 }
--- MyWorld.java
+++ MyWorld.java
@@ -18,10 +18,22 @@ public class MyWorld extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
         
+        
+        // Create the elephant object
         Elephant elephant = new Elephant();
         addObject(elephant, 300, 200);
         
+        createApple();
+    }
+    
+    /**
+     * Create a new apple at random location at top of screen
+     */
+    public void createApple()
+    {
         Apple apple = new Apple();
-        addObject(apple, 300, 0);
+        int x = Greenfoot.getRandomNumber(600);
+        int y = 0;
+        addObject(apple, x, y);
     }
 }
